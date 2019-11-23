2345012174e2ca76c3dcc6dc4925ddcf81f04339
jtankers <jim.tank.jr@gmail.com>
2019-11-23 14:21:28 -0600

    version 1.0.1

--- 2/text.js
+++ 2/text.js
@@ -1,13 +1,19 @@
 const textJson = {
     "title": "Bell's Inequalities CHSH test",
     "canHead": "Multi-mode computer simulation of Bell's Inequalities.<br><i>(Select [Mode] and [Fast])</i>",
-    "canFoot": `Sets of photons are repeatedly created with opposite polarization angles between 0° and 360°.<br>
+    "canFoot": `
+        <i>Please refer to <a target='tab' href='https://sites.google.com/site/physicschecker/unsettled-physics/testing-bells-theorem-paper'>paper</a> [I] for theoretical background.</i><br>
+        <br>
+        Sets of photons are repeatedly created with opposite polarization angles <i>(pre-set to between 0° and 360° for non-quantum modes)</i>.<br>
         Polarizers a, a′, b and b′ are set to angles of 0°, 45°, 22.5° and 67.5° respectively.<br>
-        Polarizer angles (<i>a or a′ and b or b′</i>) are randomly selected before each set of photons are measured.<br>
-        Mode may be set to:<br>
-        <span class='tab-left'><b>Quantum</b> (<i>simulated 'spooky action at a distance', random 50% detection</i>)</span><br>
-        <span class='tab-left'><b>Classic</b> (<i>local hidden variables, cos²(θ) probability match, |cos(2θ)| probability detection</i>)</span><br>
-        <span class='tab-left'><b>Karma Peny</b> (<i>local hidden variables, cos²(θ) simple match, |cos(2θ)| probability detection [VIII]</i>)</span><br>`,
+        Polarizer angles (<i>a or a′ and b or b′</i>) are randomly selected before each set of photons are measured.<br><br>
+        <b>Mode</b> may be set to:<br>
+        <span class='tab-left'><b>Quantum</b> (<i><u>polarize</u> = 'spooky action at a distance', <u>detection</u> = 50% probability</i>)</span><br>
+        <span class='tab-left'><b>Classic</b> (<i>local hidden variables, <u>polarize</u> = cos²(θ) probability, <u>detection</u> = |cos(2θ)| probability</i>)</span><br>
+        <span class='tab-left'><b>Karma Peny</b> (<i>local hidden variables, <u>polarize</u> = cos²(θ) linear, <u>detection</u> = |cos(2θ)| probability [IV]</i>)</span><br>
+        <span class='tab-left'><b>Alternate 1</b> (<i>local hidden variables, <u>polarize</u> = cos²(θ) linear, <u>detection</u> = 0.37+(0.63*|cos(2θ))| probability [Chantal Roth (2)]</i>)</span><br>
+        <span class='tab-left'><b>Alternate 2</b> (<i>local hidden variables, <u>polarize</u> = cos²(θ) probability, <u>detection</u> = 0.37+(0.63*|cos(2θ))| probability</i>)</span><br>
+        <br>`,
     "header1": "Totals",
     "header2": "Test Results: a, b (0°, 22.5°)",
     "header3": "Test Results: a, b′ (0°, 67.5°)",
@@ -21,18 +27,34 @@ const textJson = {
     "footer3": "",
     "footer4": "",
     "footer5": `
-        <p>See forum discussion at <a href='http://www.sciphysicsforums.com/spfbb1/viewtopic.php?f=6&t=417' target='new'>http://www.sciphysicsforums.com/spfbb1/viewtopic.php?f=6&t=417</a></p>
-        [I] Bell Tests <a target='tab' href='https://en.wikipedia.org/wiki/Bell_test_experiments'>https://en.wikipedia.org/wiki/Bell_test_experiments</a><br>
-        [II] Bell Original Paper <a target='tab' href='http://www.drchinese.com/David/Bell.pdf'>http://www.drchinese.com/David/Bell.pdf</a><br>
-        [III] Are Hidden-Variable Theories for Pilot-Wave Systems Possible? <a target='tab' href='https://arxiv.org/pdf/1701.08194.pdf'>https://arxiv.org/pdf/1701.08194.pdf</a><br>
-        [IV] Paper <a target='tab' href='https://sites.google.com/site/physicschecker/unsettled-physics/bell-chsh-simulation'>https://sites.google.com/site/physicschecker/unsettled-physics/bell-chsh-simulation</a><br>
-        [V] App <a target='tab' href='https://codeserver.net/bell/2'>https://codeserver.net/bell/2</a><br>
-        [VI] Code <a target='tab' href='https://github.com/tankersleyj/bell'>https://github.com/tankersleyj/bell</a><br>
-        [VII] Expected CHSH values <a target='tab' href='https://arxiv.org/pdf/1207.5103.pdf'>https://arxiv.org/pdf/1207.5103.pdf</a><br>
-        [VIII] Karma Peny, cos²(θ) match * |cos(θ)| detection, <a target='tab' href='https://www.youtube.com/watch?v=yOtsEgbg1-s'>https://www.youtube.com/watch?v=yOtsEgbg1-s</a><br>
-        Other for review: <a target='tab' href='https://arxiv.org/pdf/1604.08105.pdf'>https://arxiv.org/pdf/1604.08105.pdf</a>, 
-        <a target='tab' href='https://arxiv.org/pdf/1704.02876.pdf'>https://arxiv.org/pdf/1704.02876.pdf</a>, 
-        <a target='tab' href='https://arxiv.org/pdf/quant-ph/0304066.pdf'>https://arxiv.org/pdf/quant-ph/0304066.pdf</a><br><br>
+        <div>Polarization and Detection Graph:<br>
+        <div class='tab-left'>
+        <img src="graph.png" alt="Detection and Polarization Graph"><br>
+        <b>x</b> = θ as difference between photon polarization angle and polarizer angle<br>
+        <b>Green</b> = Detection Probability, |cos(2x)|<br>
+        <b>Red</b> = Probability of passing through polarizer (+) instead of being reflected (-), |cos²(θ)|<br>
+        </div></div><br>
+        <div>Forums:<br>
+        <div class='tab-left'>
+        (1) <a href='http://www.sciphysicsforums.com/spfbb1/viewtopic.php?f=6&t=417' target='new'>http://www.sciphysicsforums.com/spfbb1/viewtopic.php?f=6&t=417</a><br>
+        (2) <a target='tab' href='https://groups.google.com/forum/#!topic/bell_quantum_foundations/CdvTO7aIOXs'>Karma Peny Math Appears to Violate Bell</a>
+        </div></div><br>
+        <div>References:<br>
+        <div class='tab-left'>
+        [I] Paper <a target='tab' href='https://sites.google.com/site/physicschecker/unsettled-physics/testing-bells-theorem-paper'>https://sites.google.com/site/physicschecker/unsettled-physics/testing-bells-theorem-paper</a><br>
+        [II] App <a target='tab' href='https://codeserver.net/bell/2'>https://codeserver.net/bell/2</a><br>
+        [III] Code <a target='tab' href='https://github.com/tankersleyj/bell'>https://github.com/tankersleyj/bell</a><br>
+        [IV] Karma Peny, Explained & Debunked: Quantum Entanglement & Bell Test Experiments <a target='tab' href='https://www.youtube.com/watch?v=yOtsEgbg1-s'>https://www.youtube.com/watch?v=yOtsEgbg1-s</a><br>
+        </div></div><br>
+        <div>Other links and forums: <br>
+        <a class='tab-left' target='tab' href='https://en.wikipedia.org/wiki/Bell_test_experiments'>Bell Tests, Wikipedia</a><br>
+        <a class='tab-left' target='tab' href='http://www.drchinese.com/David/Bell.pdf'>Bell Original Paper</a><br>
+        <a class='tab-left' target='tab' href='https://arxiv.org/pdf/1701.08194.pdf'>Are Hidden-Variable Theories for Pilot-Wave Systems Possible?</a><br>
+        <a class='tab-left' target='tab' href='https://arxiv.org/pdf/1604.08105.pdf'>https://arxiv.org/pdf/1604.08105.pdf</a><br> 
+        <a class='tab-left' target='tab' href='https://arxiv.org/pdf/1704.02876.pdf'>https://arxiv.org/pdf/1704.02876.pdf</a><br>
+        <a class='tab-left' target='tab' href='https://arxiv.org/pdf/quant-ph/0304066.pdf'>https://arxiv.org/pdf/quant-ph/0304066.pdf</a><br>
+        <a class='tab-left' target='tab' href='https://arxiv.org/pdf/1207.5103.pdf'>https://arxiv.org/pdf/1207.5103.pdf</a><br>
+        </div><br>
         (<i>please excuse the simple 2D graphics which rotate polarizers in the x-y plane instead of the y-z plane</i>)`
 }
 const reportExpected = {
@@ -40,30 +62,35 @@ const reportExpected = {
         "Classic": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Karma_Peny": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Quantum": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
-        "Test": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
+        "Alternate_1": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
+        "Alternate_2": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
         },
     "2": {
         "Classic": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"],
         "Karma_Peny": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"],
         "Quantum": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"],
-        "Test": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"]
+        "Alternate_1": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"],
+        "Alternate_2": ["cos²(67.5)/2 = 7%", "(1-cos²(67.5))/2 = 43%", "(1-cos²(67.5))/2 = 43%", "cos²(67.5)/2 = 7%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=-0.71"]
         },
     "3": {
         "Classic": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Karma_Peny": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Quantum": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
-        "Test": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
+        "Alternate_1": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
+        "Alternate_2": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
         },
     "4": {
         "Classic": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Karma_Peny": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
         "Quantum": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
-        "Test": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
+        "Alternate_1": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"],
+        "Alternate_2": ["cos²(22.5)/2 = 43%", "(1-cos²(22.5))/2 = 7%", "(1-cos²(22.5))/2 = 7%", "cos²(22.5)/2 = 43%", "([1]-[2]-[3]+[4])/([1]+[2]+[3]+[4])=0.71"]
         },
     "5": {
         "Classic": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected=75%"],
         "Karma_Peny": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected (|1-cos(2θ)|)"],
         "Quantum": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected=75%"],
-        "Test": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected (|1-cos(2θ)|)"]
+        "Alternate_1": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected (|1-cos(2θ)|)"],
+        "Alternate_2": ["0.707", "-0.707", "0.707", "0.707", "E1-E2+E3+E4=2.83 (>=2)", "not detected (|1-cos(2θ)|)"]
         }
 }
