7de16e0acf36b55cadf382b1bf2ff2815e819b5d
jtankers <jim.tank.jr@gmail.com>
2019-11-24 17:28:58 -0600

    version 1.1.04

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
   <body>
       <div class='content'>
           <p>
-            <b>Bell Simulations:</b><br>
-            1. <a href='2' target='_new'>Testing Bell's Theorem <small>(<i>CHSH violations with Realistic & Karma Peny math</i>)</small></a><br>
-            2. <a href='2/index-alt.php' target='_new'>Alternate Link <small>(<i>alternate location for Testing Bell's Theorem</i>)</small></a><br>
-            3. <a href='1' target='_new'>Early Prototype <small>(<i>Non-productive 'Easy Math' Simulation</i>)</small></a><br>
+            <b>Computer Experiments:</b><br>
+            1. <a href='2' target='_new'>Testing Bell's Theorem - CHSH Experiment <small>(<i>Bell violations with Realistic & Karma Peny math</i>)</small></a><br>
+            <br>
+            <b>Other Links:</b><br>
+            a. <a href='https://sites.google.com/site/physicschecker/references/testing-bell-chsh-experiment' target='_new'>Alternate CHSH Link</small></a><br>
+            b. <a href='1' target='_new'>Early Prototype <small>(<i>Non-productive 'Easy Math' experiment</i>)</small></a><br>
           </p>          
       </div>
   </body>
